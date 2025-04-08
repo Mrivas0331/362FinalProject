d65b8d1c88d14960b00981da4c51b6ee78db0682
Max Rivas <Rivas.max11@csu.fullerton.edu>
2025-04-08 15:44:32 -0700

    changed directories around to fit new express

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ storage.init();
 
 const app = express();
 const port = 5500;
-app.use(express.static(path.join(__dirname, "src")));
+app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
