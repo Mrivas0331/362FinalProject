5aa24885dc438007db7a1ac943efee95c714f0d4
Marco Macias <marcomacias410@csu.fullerton.edu>
2025-05-03 19:34:16 -0700

    not finished but add product is mostly finished, just need to fix loading the item and scaling it so it doens't overlap with footer

--- public/addproduct.js
+++ public/addproduct.js
@@ -40,11 +40,11 @@ async function loadData() {
     const response = await fetch(`/get/${key}`);
     const data = await response.json();
     if (response.ok) {
-        document.getElementById('keyout').innerText = `${data.key}\n`;
-        document.getElementById('nameout').innerText = `${data.value.name}\n`;
-        document.getElementById('priceout').innerText = `${data.value.price}\n`;
-        document.getElementById('descout').innerText = `${data.value.desc}\n`;
-        document.getElementById('categoryout').innerText = `${data.value.category}\n`;
+        document.getElementById('keyout').innerText = `Key: ${data.key}\n`;
+        document.getElementById('nameout').innerText = `Name: ${data.value.name}\n`;
+        document.getElementById('priceout').innerText = `Price: $${data.value.price}\n`;
+        document.getElementById('descout').innerText = `Desc: ${data.value.desc}\n`;
+        document.getElementById('categoryout').innerText = `Cateogry: ${data.value.category}\n`;
         document.getElementById('imgout').src = `${data.value.img}`;
     } else {
         document.getElementById('result').innerText = data.error;
